c646cd18c5d69ac89d96b04098dcc77c643a4018
Wan Youwen <1756429307@qq.com>
2017-12-26 20:39:16 +0800

    虚函数 继承 多态

---
+++ learn_cpp/class_Virtual_Table.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+ 
+//基类 Base1
+class Base1 {
+public:
+            virtual void f() { cout << "Base1::f" << endl; }
+            virtual void g() { cout << "Base1::g" << endl; }
+            virtual void h() { cout << "Base1::h" << endl; }
+ 
+};
+
+//基类 Base2 
+class Base2 {
+public:
+            virtual void f() { cout << "Base2::f" << endl; }
+            virtual void g() { cout << "Base2::g" << endl; }
+            virtual void h() { cout << "Base2::h" << endl; }
+};
+
+//基类 Base3 
+class Base3 {
+public:
+            virtual void f() { cout << "Base3::f" << endl; }
+            virtual void g() { cout << "Base3::g" << endl; }
+            virtual void h() { cout << "Base3::h" << endl; }
+};
+ 
+//子类 继承于 父类 Base1  Base2  Base3 
+class Derive : public Base1, public Base2, public Base3 {
+public:
+            virtual void f() { cout << "Derive::f" << endl; }  //覆盖了 父类的 函数f()
+            virtual void g1() { cout << "Derive::g1" << endl; }//独有的  子类 函数
+};
+ 
+ 
+typedef void(*Fun)(void);//函数指针
+ 
+int main()
+{
+            Fun pFun = NULL;
+ 
+            Derive d;//子类
+            int** pVtab = (int**)&d;// 虚函数表 函数指针数组
+ 
+//父类Base1的 虚函数表 Base1's vtable
+//  |Derive::f()|Base1::g()|Base1::h()|Derive::g1()|.|
+
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+0);
+            pFun = (Fun)pVtab[0][0];
+            pFun();
+ 
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+1);
+            pFun = (Fun)pVtab[0][1];
+            pFun();
+ 
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+2);
+            pFun = (Fun)pVtab[0][2];
+            pFun();
+ 
+   
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+3);
+            pFun = (Fun)pVtab[0][3];
+            pFun();
+ 
+           //虚函数表 尾部 结束符 |.|
+            pFun = (Fun)pVtab[0][4];
+            cout<<pFun<<endl;
+ 
+ // 父类 Base2 虚函数表
+ //|Derive::f()|Base2::g()|Base2::h()|.|
+ //Base2's vtable
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+0);
+            pFun = (Fun)pVtab[1][0];
+            pFun();
+ 
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+1);
+            pFun = (Fun)pVtab[1][1];
+            pFun();
+
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+2);
+            pFun = (Fun)pVtab[1][2];
+            pFun();
+
+            pFun = (Fun)pVtab[1][3];
+            cout<<pFun<<endl;
+ 
+
+ // 父类 Base3 虚函数表
+ //|Derive::f()|Base3::g()|Base3::h()|.|
+ //Base3's vtable
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+0);
+            pFun = (Fun)pVtab[2][0];
+            pFun();
+ 
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+1);
+            pFun = (Fun)pVtab[2][1];
+            pFun();
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+2);
+            pFun = (Fun)pVtab[2][2];
+            pFun();
+           //虚函数表 尾部 结束符 |.|
+            pFun = (Fun)pVtab[2][3];
+            cout<<pFun<<endl;
+ 
+            return 0;
+}
+
