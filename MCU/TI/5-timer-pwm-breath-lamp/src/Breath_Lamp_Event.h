8e97f80cd93537792e80888028bca41ed65ca6f7
Wan Youwen <1756429307@qq.com>
2018-06-18 23:05:16 +0800

    Create Breath_Lamp_Event.h

---
+++ MCU/TI/5-timer-pwm-breath-lamp/src/Breath_Lamp_Event.h
@@ -0,0 +1,6 @@
+// Breath_Lamp_Event.c 对应的头文件Breath_Lamp_Event.h 。
+//=================Breath_Lamp_Event.h=============
+   #ifndef GLOBAL_H_
+   #define GLOBAL_H_
+   extern void WDT_Ontime();                 //WDT 中断事件
+   #endif /* GLOBAL_H_ */
