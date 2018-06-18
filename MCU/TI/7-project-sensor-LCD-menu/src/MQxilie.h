3029f229b4b16ff35058920bbd9e9b0c9f98acd3
Wan Youwen <1756429307@qq.com>
2018-06-19 00:03:30 +0800

    Create MQxilie.h

---
+++ MCU/TI/7-project-sensor-LCD-menu/src/MQxilie.h
@@ -0,0 +1,15 @@
+
+#ifndef MQxilie_H_
+#define MQxilie_H_
+
+extern void MQ1_adc10_init(void);
+extern void MQ2_adc10_init(void);
+extern void PM_adc10_init(void);
+extern void MQ1_Vol_Con(void);
+extern void MQ2_Vol_Con(void);
+extern void PM_Vol_Con(void);
+extern void MQ1_printf(void);
+extern void MQ2_printf(void);
+extern void PM_printf(void);
+extern long COppm,CH4ppm,Benppm,jqppm,PMppm;
+#endif /* DHT11_H_*/
