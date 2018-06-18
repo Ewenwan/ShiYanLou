1f48fabac69415ae3e772bcc59b4a6b95575207c
Wan Youwen <1756429307@qq.com>
2018-06-18 23:05:56 +0800

    Create Breath_Lamp_Global.h

---
+++ MCU/TI/5-timer-pwm-breath-lamp/src/Breath_Lamp_Global.h
@@ -0,0 +1,8 @@
+/**************  用CCS 生成头文件会自动加上条件编译语句，可以防止头文件冲突。
+Breath_Lamp_Global.c 对应的头文件Breath_Lamp_Global.h 。
+*********************************************************/
+   /******************************************Breath_Lamp_Global.h *******************************************/
+   #ifndef  BREATH_LAMP_GLOBAL_H_
+   #define  BREATH_LAMP_GLOBAL_H_
+   extern int  Bright;                       //系统全局变量占空比Bright
+   #endif                                    /* BREATH_LAMP_GLOBAL_H_ */
