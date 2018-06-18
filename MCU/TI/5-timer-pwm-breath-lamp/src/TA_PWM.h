6b6b1c662a00f07780c716d5a5cc648a00e4469d
Wan Youwen <1756429307@qq.com>
2018-06-18 23:04:29 +0800

    Create TA_PWM.h

---
+++ MCU/TI/5-timer-pwm-breath-lamp/src/TA_PWM.h
@@ -0,0 +1,10 @@
+//Timer_A 库函数文件  库函数的头文件  ======================== TA_PWM.h====================//
+  #ifndef TA_PWM_H_
+  #define TA_PWM_H_
+  extern char TA0_PWM_Init();
+  extern char TA0_PWM_SetPeriod();
+  extern char TA0_PWM_SetPermill();
+  extern char TA1_PWM_Init();
+  extern char TA1_PWM_SetPeriod();
+  extern char TA1_PWM_SetPermill();
+  #endif /* TA_PWM_H_ */
