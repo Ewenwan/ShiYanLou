26efbe83ed3a73e75ade90cbb1caea83435f9daa
Wan Youwen <1756429307@qq.com>
2018-06-18 23:38:34 +0800

    Create DHT11.h

---
+++ MCU/TI/qudong/DHT11.h
@@ -0,0 +1,9 @@
+
+#ifndef DHT11_H_
+#define DHT11_H_
+
+extern unsigned char start_DHT11();
+extern unsigned char DHT11_ReadChar();
+extern void DHT11_UART_init();
+extern void DHT11_port_init();
+#endif /* DHT11_H_*/
