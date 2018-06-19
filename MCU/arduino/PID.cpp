4fbd3fad5b0728752a5cfee0079fdd42df0d35e0
Wan Youwen <1756429307@qq.com>
2018-06-19 11:36:25 +0800

    修改

---
+++ MCU/arduino/PID.cpp
@@ -0,0 +1,114 @@
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include"PID.h"
+#include<PWM.h>
+//输出限幅
+void  PID::Left_Speed_Limit()
+{   
+    if(Left_Output>PWM_MAX)  Left_Output=PWM_MAX;
+    if(Left_Output<PWM_MIN)   Left_Output=PWM_MIN;      
+  }
+//输出限幅  
+void  PID::Right_Speed_Limit()
+{   if(Right_Output>PWM_MAX)  Right_Output=PWM_MAX;
+    if(Right_Output<PWM_MIN)  Right_Output=PWM_MIN;      
+  }
+//红外测距口
+void PID::Infraed_read()
+{
+        Infrared_value[0]=analogRead(Infrared_Forward);       //AD读取一次数据
+        Infrared_value[0]=13146*pow(Infrared_value[0],-1.161);//幂函数 电压转换到 距离
+        /*
+		规范化
+		*/
+		if(Infrared_value[0]>=99){
+                Infrared_value[0]=99;
+            }
+        else if  (Infrared_value[0]<=10){
+                Infrared_value[0]=10;
+            }
+              
+        Infrared_value[1]=analogRead(Infrared_Left);        //AD读取一次数据
+        Infrared_value[1]=13146*pow(Infrared_value[1],-1.161);
+        if(Infrared_value[1]>=99){
+                Infrared_value[1]=99;
+            }
+        else if (Infrared_value[1]<=10){
+                Infrared_value[1]=10;
+            } 
+
+        Infrared_value[2]=analogRead(Infrared_Right);        //AD读取一次数据
+        Infrared_value[2]=13146*pow(Infrared_value[2],-1.161);
+        if(Infrared_value[2]>=99){
+                Infrared_value[2]=99;
+            }
+        else if (Infrared_value[2]<=10){
+                Infrared_value[2]=10;
+            } 
+              
+        Infrared_value[3]=analogRead(Infrared_Back);        //AD读取一次数据
+        Infrared_value[3]=(10500*Infrared_value[3]/8192);   //电池电压转换
+        if(Infrared_value[3]>=1500){
+                Infrared_value[3]=1200;
+            }
+        else if (Infrared_value[3]<=10){
+                Infrared_value[3]=1200;
+            } 
+
+  }
+
+ void PID::PID_cacu_control()
+ {
+   Left_Input=countL;// 类内直接调用
+   Err_Left[0]=Left_Speedwant-Left_Input;//本次误差 设定值-反馈值
+   Error_i_Left+=Err_Left[0];//积分误差和
+   Err_Left[1]=Err_Left[0];//上次误差
+
+   Left_Output=Kp_Left*Err_Left[0]+Kd_Left*(Err_Left[0]-Err_Left[1])+Ki_Left*Error_i_Left;  
+   Left_Speed_Limit();//限幅
+   Left_last=Left_Output;
+
+   Right_Input=countR; 
+   Err_Right[0]=Right_Speedwant-Right_Input;//本次误差 设定值-反馈值
+   Error_i_Right+=Err_Right[0];
+   Err_Right[1]=Err_Right[0];
+
+   Right_Output=Kp_Right*(Err_Right[0])+Kd_Right*(Err_Right[0]-Err_Right[1])+Ki_Right*Error_i_Right;             
+   Right_Speed_Limit();//限幅 
+   Right_last=Right_Output;
+
+   if(Right_Speedwant==0)
+   {
+		 pwmWriteHR(Right_Motor_pwm, 0);                     //左电机控制频率初始化
+		 Right_Output=0;
+		 Err_Right[2]=Err_Right[1]=Err_Right[0]=0;
+		 Error_i_Right=0;
+		 Right_last=0;
+		 
+   }
+   else
+   {
+		 pwmWriteHR(Right_Motor_pwm, Right_Output);          //左电机控制频率初始化
+   }
+
+	if(Left_Speedwant==0)
+   {
+		pwmWriteHR(Left_Motor_pwm, 0);                       //右电机控制占孔比初始化
+		Left_Output=0;
+		Err_Left[2]=Err_Left[1]=Err_Left[0]=0;
+		Error_i_Left=0;
+		Left_last=0;
+   }
+   else
+   {
+		pwmWriteHR(Left_Motor_pwm, Left_Output);            //右电机控制占孔比初始化
+	}
+  }
+
+
+
+
---
+++ MCU/arduino/PID.h
@@ -0,0 +1,53 @@
+#ifndef PID_H
+#define PID_H
+
+
+#define PWM_SCOPE   65535//脉冲 
+#define PWM_MAX      PWM_SCOPE*0.9
+#define PWM_MIN      0
+
+// 红外传感器
+#define Infrared_Forward A0
+#define Infrared_Left    A1
+#define Infrared_Right   A2
+#define Infrared_Back    A5
+
+//电机PWM输出  注意要与 ino文件一致
+#define Left_Motor_pwm      11
+#define Right_Motor_pwm     12
+
+class PID  //定义类主体及类名
+{
+  public:  //以下定义为公共方法
+     volatile long   Left_Output=0,Right_Output=0,Left_Speedwant=0,Right_Speedwant=0;//pid输出  和 速度对应的 期望脉冲值
+     volatile unsigned long countL = 0;     //左电机脉冲值计数
+     volatile unsigned long countR = 0;     //右电机脉冲值计数 
+     volatile double Infrared_value[4]={0}; 
+	   volatile long Kp_Left=90, Ki_Left=10, Kd_Left=5; 
+	   volatile long Kp_Right=90, Ki_Right=10, Kd_Right=5;
+	   volatile long Err_Left[2]={0},Error_i_Left=0,Left_last=0; 
+     volatile long Err_Right[2]={0},Error_i_Right=0,Right_last=0;          
+    
+     void Infraed_read();//读取红外传感器
+     void PID_cacu_control();//计算pid输出
+     void  Left_Speed_Limit();//输出限幅
+     void  Right_Speed_Limit();
+	 
+  private:  //以下定义为私有方法
+     volatile long Left_Input=0;                 
+//Left_Speedwant为左电机设定速度值，Left_Input为左电机实际速度
+//Left_Output为左电机控制占空比
+// const long Kp_Left=90, Ki_Left=10, Kd_Left=5;                                
+//左电机PID参数
+
+    volatile long Right_Input=0;           
+//Right_Speedwant为右电机设定速度值，Right_Input为右电机实际速度 
+//Right_Output为右电机控制占空比
+  // const long Kp_Right=90, Ki_Right=10, Kd_Right=5;     //对不同的电机需要修改 应改为公共方法                  
+//右电机PID参数
+    
+};
+
+#endif
+
+
