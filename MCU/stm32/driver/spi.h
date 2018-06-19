c0f0533c16cd14207373068dcb768804dccc6445
Wan Youwen <1756429307@qq.com>
2018-06-19 09:38:06 +0800

    Create spi.h

---
+++ MCU/stm32/driver/spi.h
@@ -0,0 +1,21 @@
+#ifndef __SPI_H
+#define __SPI_H
+#include "sys.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK战舰STM32开发板
+//SPI驱动 代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2012/9/9
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved									  
+//////////////////////////////////////////////////////////////////////////////////
+				  	    													  
+void SPI2_Init(void);			 //初始化SPI口
+void SPI2_SetSpeed(u8 SpeedSet); //设置SPI速度   
+u8 SPI2_ReadWriteByte(u8 TxData);//SPI总线读写一个字节
+		 
+#endif
