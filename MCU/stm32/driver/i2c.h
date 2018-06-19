44b64155df1b0204507ff41c9bb93266df8b5c6a
Wan Youwen <1756429307@qq.com>
2018-06-19 09:33:35 +0800

    Create i2c.h

---
+++ MCU/stm32/driver/i2c.h
@@ -0,0 +1,26 @@
+#ifndef __MYIIC_H
+#define __MYIIC_H
+#include "sys.h"
+
+//IO方向设置
+#define SDA_IN()  {GPIOB->CRH&=0XFFFF0FFF;GPIOB->CRH|=8<<12;}
+#define SDA_OUT() {GPIOB->CRH&=0XFFFF0FFF;GPIOB->CRH|=3<<12;}
+
+//IO操作函数	 
+#define IIC_SCL    PBout(10) //SCL
+#define IIC_SDA    PBout(11) //SDA	 
+#define READ_SDA   PBin(11)  //输入SDA 
+
+//IIC所有操作函数
+void IIC_Init(void);                //初始化IIC的IO口				 
+void IIC_Start(void);				//发送IIC开始信号
+void IIC_Stop(void);	  			//发送IIC停止信号
+void IIC_Send_Byte(u8 txd);			//IIC发送一个字节
+u8 IIC_Read_Byte(unsigned char ack);//IIC读取一个字节
+u8 IIC_Wait_Ack(void); 				//IIC等待ACK信号
+void IIC_Ack(void);					//IIC发送ACK信号
+void IIC_NAck(void);				//IIC不发送ACK信号
+
+//void IIC_Write_One_Byte(u8 daddr,u8 addr,u8 data);
+//u8 IIC_Read_One_Byte(u8 daddr,u8 addr);	  
+#endif
