b6b25868aa11edf5d2ec6dbd73baf33734949e0b
Wan Youwen <1756429307@qq.com>
2018-08-23 23:36:38 +0800

    Update class_Virtual_Table.cpp

--- learn_cpp/class_Virtual_Table.cpp
+++ learn_cpp/class_Virtual_Table.cpp
@@ -5,21 +5,30 @@ C++的多态性用一句话概括就是：
 Derive::f// 被覆盖的函数 存在于 对应被覆盖的 父类的 虚函数表中
 Base1::g
 Base1::h
-Derive::g1// 子类新增添的 函数 只存在于 继承源自的 第一个子类的虚函数表中 
-1
+Derive::g1// 子类新增添的 函数 只存在于 继承源自的 第一个子类的虚函数表中
+Derive::h1// 子类新增添的 函数 只存在于 继承源自的 第一个子类的虚函数表中
+1 
 Derive::f// 被覆盖的函数 存在于 对应被覆盖的 父类的 虚函数表中
 Base2::g
 Base2::h
-1
+1 
 Derive::f// 被覆盖的函数 存在于 对应被覆盖的 父类的 虚函数表中
 Base3::g
 Base3::h
-0
+0 
+
+// 子类 Derive 继承于 父类3ase1 Base2 Base3
+// 子类 内存空间 中的 虚函数表
+ |Derive::f()|Base1::g()|Base1::h()|Derive::g1()|Derive::h1()|1|
+ |Derive::f()|Base2::g()|Base2::h()|1|
+ |Derive::f()|Base3::g()|Base3::h()|0|
+////////////////////////////////////////
+ 
 */
 #include <iostream>
 using namespace std;
  
-//基类 Base1
+// 基类(父类) Base1
 class Base1 {
 public:
             virtual void f() { cout << "Base1::f" << endl; }
@@ -28,7 +37,7 @@ public:
  
 };
 
-//基类 Base2 
+// 基类(父类) Base2 
 class Base2 {
 public:
             virtual void f() { cout << "Base2::f" << endl; }
@@ -36,7 +45,7 @@ public:
             virtual void h() { cout << "Base2::h" << endl; }
 };
 
-//基类 Base3 
+// 基类(父类) Base3 
 class Base3 {
 public:
             virtual void f() { cout << "Base3::f" << endl; }
@@ -44,14 +53,16 @@ public:
             virtual void h() { cout << "Base3::h" << endl; }
 };
  
-//子类 继承于 父类 Base1  Base2  Base3  子类base  Derive共有 继承自父类的函数 （3-1）*3 和 自己的2个函数
+// 子类(派生类) 继承于 父类 Base1  Base2  Base3  子类Derive 有 继承自父类的函数 （3-1）*3 和 自己的3个函数
 class Derive : public Base1, public Base2, public Base3 {
 public:
-            virtual void f() { cout << "Derive::f" << endl; }  //覆盖了 父类的 函数f()
-            virtual void g1() { cout << "Derive::g1" << endl; }//独有的  子类 函数
+            virtual void f() { cout << "Derive::f" << endl; }  // 覆盖了 3个父类的 虚函函数f()
+            virtual void g1() { cout << "Derive::g1" << endl; }// 独有的  子类 函数
+            virtual void h1() { cout << "Derive::h1" << endl; }// 独有的  子类 函数
 };
  
-typedef void(*Fun)(void);// 类型别名定义 Fun是函数指针 指向一个函数类型为 void(void)的函数，即返回类型为void 形参参数也为void
+typedef void(*Fun)(void);// 类型别名定义 Fun是函数指针 
+// 指向一个函数类型为 void(void)的函数，即返回类型为void 形参参数也为void
  
 int main()
 {
@@ -61,27 +72,30 @@ int main()
             int** pVtab = (int**)&d;// 虚函数表 函数指针数组
  
 //父类Base1的 虚函数表 Base1's vtable
-//  |Derive::f()|Base1::g()|Base1::h()|Derive::g1()|.|
+//  |Derive::f()|Base1::g()|Base1::h()|Derive::g1()|Derive::h1()|.|
 
             //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+0);
             pFun = (Fun)pVtab[0][0];
-            pFun();
+            pFun();// 执行 Derive::f()   打印 Derive::f
  
             //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+1);
             pFun = (Fun)pVtab[0][1];
-            pFun();
+            pFun();// 执行 Base1::g()    打印 Base1::g
  
             //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+2);
             pFun = (Fun)pVtab[0][2];
-            pFun();
- 
+            pFun();// 执行 Base1::h()    打印 Base1::h
    
             //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+3);
             pFun = (Fun)pVtab[0][3];
-            pFun();
+            pFun();// 执行 Derive::g1()   打印 Derive::g1
  
-           //虚函数表 尾部 结束符 |.|
+            //pFun = (Fun)*((int*)*(int*)((int*)&d+0)+4);
             pFun = (Fun)pVtab[0][4];
+            pFun();// 执行 Derive::h1()   打印 Derive::h1
+            
+            //虚函数表 尾部 结束符 |.|
+            pFun = (Fun)pVtab[0][5];
             cout<<pFun<<endl;
  
  // 父类 Base2 虚函数表
@@ -89,16 +103,17 @@ int main()
  //Base2's vtable
             //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+0);
             pFun = (Fun)pVtab[1][0];
-            pFun();
+            pFun();// 执行  Derive::f()  打印 Derive::f
  
             //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+1);
             pFun = (Fun)pVtab[1][1];
-            pFun();
+            pFun();// 执行  Base2::g()   打印 Base2::g
 
             //pFun = (Fun)*((int*)*(int*)((int*)&d+1)+2);
             pFun = (Fun)pVtab[1][2];
-            pFun();
-
+            pFun();// 执行  Base2::h()   打印 Base2::h
+ 
+            //虚函数表 尾部 结束符 |.|
             pFun = (Fun)pVtab[1][3];
             cout<<pFun<<endl;
  
@@ -108,14 +123,16 @@ int main()
  //Base3's vtable
             //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+0);
             pFun = (Fun)pVtab[2][0];
-            pFun();
+            pFun();// 执行  Derive::f()  打印 Derive::f
  
             //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+1);
             pFun = (Fun)pVtab[2][1];
-            pFun();
+            pFun();// 执行  Base3::g()   打印 Base3::g
+ 
             //pFun = (Fun)*((int*)*(int*)((int*)&d+2)+2);
             pFun = (Fun)pVtab[2][2];
-            pFun();
+            pFun();// 执行  Base3::h()   打印 Base3::h
+ 
            //虚函数表 尾部 结束符 |.|
             pFun = (Fun)pVtab[2][3];
             cout<<pFun<<endl;
