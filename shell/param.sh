40573ae9d97e4d41b69ef0171298bd1ceaec2d1f
Wan Youwen <1756429307@qq.com>
2018-10-07 16:23:25 +0800

    Create param.sh

---
+++ shell/param.sh
@@ -0,0 +1,16 @@
+#########################################################################
+# File Name: param.sh
+# Author: He Jieting
+# mail: rambo@mail.ustc.edu.cn
+# Created Time: 2016年01月06日 星期三 19时14分28秒
+#########################################################################
+#!/bin/bash
+
+echo "Shell params test!"
+echo "First param:  $1"
+echo "Second Param: $2"
+echo "Thrid Param:  $3"
+
+echo "number of param: $#"
+echo "all param: $*"
+echo "PID: $$"
