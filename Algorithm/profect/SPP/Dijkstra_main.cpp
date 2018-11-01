4dee0dbe36b039bd3b359847f95f36a852c86323
Wan Youwen <1756429307@qq.com>
2018-11-01 20:25:25 +0800

    Create Dijkstra_main.cpp

---
+++ Algorithm/profect/SPP/Dijkstra_main.cpp
@@ -0,0 +1,43 @@
+/*
+输入：
+
+6 8
+1 3 10
+1 5 30
+1 6 100
+2 3 5
+3 4 50
+4 6 10
+5 6 60
+5 4 20
+
+
+*/
+
+#include"Dijkstra.h"
+
+
+//检验输入边数和顶点数的值是否有效，可以自己推算为啥：
+//顶点数和边数的关系是：((Vexnum*(Vexnum - 1)) / 2) < edge
+bool check(int Vexnum, int edge) {
+    if (Vexnum <= 0 || edge <= 0 || ((Vexnum*(Vexnum - 1)) / 2) < edge)
+        return false;
+    return true;
+}
+int main() {
+    int vexnum; int edge;
+
+    cout << "输入图的顶点个数和边的条数：" << endl;
+    cin >> vexnum >> edge;
+    while (!check(vexnum, edge)) {
+        cout << "输入的数值不合法，请重新输入" << endl;
+        cin >> vexnum >> edge;
+    }
+    Graph_DG graph(vexnum, edge);
+    graph.createGraph();
+    graph.print();
+    graph.Dijkstra(1);
+    graph.print_path(1);
+    system("pause");
+    return 0;
+}
