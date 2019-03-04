fbe025835cdca17fe0da7f1c7f4e42ce519b8ee2
Wan Youwen <1756429307@qq.com>
2019-03-04 22:37:32 +0800

    Update pwm呼吸灯(多任务).c

--- MCU/arduino/project/pwm呼吸灯(多任务).c
+++ MCU/arduino/project/pwm呼吸灯(多任务).c
@@ -1,3 +1,34 @@
+/*
+// 两盏led分别闪亮============
+unsigned long int ev1 = 0;
+unsigned long int ev2 = 0;
+int led_1_va1 = 0;
+int led_2_val = 0;
+
+void setup()
+{
+	pinMode(13, OUTPUT);
+	pinMode(11, OUTPUT);
+	ev1 = millis();
+	ev2 = millis();
+}	
+
+void loop()
+{
+	if (millis() - ev1 > 500) {
+		digitalWrite(13, led_1_va1);
+		led_1_va1 = ~led_1_va1;
+		ev1 = millis();
+	}else if (millis() - ev2 > 2000) {
+		digitalWrite(11, led_2_val);
+		led_2_val = ~led_2_val;
+		ev2 = millis();
+	}
+}
+*/
+
+
+
 int brightness = 0;
 int brightness_b = 0; //控制两盏led的亮度
 
@@ -58,3 +89,31 @@ void loop()
 	}
 
 }
+
+/*
+// 定时器闪灯=========
+#include <MsTimer2.h>
+int led_val = 0;
+
+void blink_other_led(void) {
+	digitalWrite(11, led_val);
+	led_val = ~led_val;
+} //这个函数和多任务的服务进行绑定。
+
+void setup()
+{
+	pinMode(13, OUTPUT);
+	pinMode(11, OUTPUT);
+	MsTimer2::set(1000, blink_other_led); //采用MsTimer2这个库进行多任务操作，每1000ms执行一次函数
+											//blink_other_led
+	MsTimer2::start(); //启动多任务  
+}
+
+void loop()
+{
+	digitalWrite(13, HIGH);
+	delay(500);
+	digitalWrite(13, LOW);
+	delay(500); //这里的loop正常写delay，当做这里有一个占用CPU很大的事件，因为这里进行长时间的延时
+}
+*/
